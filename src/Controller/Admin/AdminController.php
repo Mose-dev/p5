9b6db8daff3f9ad9f9342197da32ab499eca6d23
moise <moisevelez543@gmail.com>
2021-06-16 16:28:23 +0200

    Création de l'interface admin

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
 use App\Entity\Annonces;
+use App\Form\ProfilType;
 use App\Entity\Categories;
 use App\Form\AnnoncesType;
 use App\Form\CategoriesType;
 use App\Repository\UserRepository;
 use App\Repository\AnnoncesRepository;
 use App\Repository\CategoriesRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 #[Route('/admin', name: 'admin_')]
 class AdminController extends AbstractController
@@ -24,10 +28,95 @@ class AdminController extends AbstractController
             'controller_name' => 'AdminController',
         ]);
     }
+
+    //Profil admin
+    
     #[Route('/profil', name: 'profil')]
     public function profil(): Response
     {
-        return $this->render('admin/user/profil.html.twig');
+        return $this->render('admin/profil/profil.html.twig');
+    }
+    
+    //Modification du profil
+    
+    /**
+     * @Route("/{id}/edit", name="edit_profil", methods={"GET","POST"})
+     */
+    public function editProfil(Request $request, User $user, EntityManagerInterface $entityManager, UserPasswordEncoderInterface $encoder): Response
+    {
+        $form = $this->createForm(ProfilType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //$hashed = $encoder->encodePassword($user, $user->getPassword());
+
+            //$user->setPassword($hashed); 
+
+            $this->getDoctrine()->getManager()->flush();
+
+            $this->addFlash("success", "Profil mis à jour avec succès");
+
+            return $this->redirectToRoute('admin_profil');
+        }
+
+        return $this->render('admin/profil/editprofil.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
+    
+    //Modification du mot de passe profil
+   
+    #[Route('/password/reset', name: 'password_reset', methods: ['GET', 'POST'])]
+    public function newPassword(Request $request, UserPasswordEncoderInterface $PasswordEncoder)
+    {
+        
+        if ($request->isMethod('POST')) {
+            $em = $this->getDoctrine()->getManager();
+
+            $user = $this->getUser();
+
+            if($request->request->get('pass') == $request->request->get('pass2'))
+            {
+                $user->setPassword($PasswordEncoder->encodePassword($user, $request->request->get('pass')));
+                $em->flush();
+                $this->addFlash('success', 'Mot de passe mis à jour avec succès');
+                    
+                return  $this->redirectToRoute('admin_profil');
+            }else
+            {
+                $this->addFlash('error', 'Les mots de passe ne sont pas identiques'); 
+            }
+        }
+            
+            return $this->render('admin/profil/editpass.html.twig');
+    }
+    #[Route('/{id}/edit/annonces/profil', name: 'annonces_edit_profil', methods: ['GET', 'POST'])]
+    public function editerAnnoncesProfil(Request $request, Annonces $annonce): Response
+    {
+        $form = $this->createForm(AnnoncesType::class, $annonce);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+            return $this->redirectToRoute('admin_profil');
+        }
+
+        return $this->render('admin/profil/editannonce.html.twig', [
+            'annonce' => $annonce,
+            'form' => $form->createView(),
+        ]);
+    }
+    #[Route('/{id}/delete', name: 'annonces_delete_profil', methods: ['POST'])]
+    public function deleteAnnoncesProfil(Request $request, Annonces $annonce): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$annonce->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($annonce);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('admin_profil');
     }
 
     //Gestion des catégories
@@ -67,7 +156,7 @@ class AdminController extends AbstractController
             'category' => $category,
         ]);
     }
-    #[Route('/{id}/edit', name: 'categories_edit', methods: ['GET', 'POST'])]
+    #[Route('/categories/{id}/edit', name: 'categories_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Categories $category): Response
     {
         $form = $this->createForm(CategoriesType::class, $category);
@@ -103,7 +192,7 @@ class AdminController extends AbstractController
      */
     public function indexUser(UserRepository $userRepository): Response
     {
-        return $this->render('admin/user/index.html.twig', [
+        return $this->render('admin/users/index.html.twig', [
             'users' => $userRepository->findAll(),
         ]);
     }
@@ -122,6 +211,27 @@ class AdminController extends AbstractController
     {
         return $this->render('admin/annonces/show.html.twig', [
             'annonce' => $annonce,
+        ]);
+    }
+
+    #[Route('/annonces/new', name: 'annonces_new', methods: ['GET', 'POST'])]
+    public function newAnnonce(Request $request)
+    {
+        $annonce = new Annonces();
+        $form = $this->createForm(AnnoncesType::class, $annonce);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($annonce);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_profil');
+        }
+         
+        return $this->render('admin/profil/newannonce.html.twig', [
+            'annonce' => $annonce,
+            'form' => $form->createView(),
         ]);
     }
     #[Route('/{id}/edit/annonces', name: 'annonces_edit', methods: ['GET', 'POST'])]
